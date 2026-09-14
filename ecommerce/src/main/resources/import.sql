f43f4513535cfb083832e4f669eccfb4acaed806
CarlosRBH <carlosbeltramo2002@gmail.com>
2026-09-14 23:12:24 +0000

    aula 5

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into categoria (nome, descricao) value ('Livros','Livros Técnicos');
+insert into categoria (nome, descricao) value ('Eletrônicos','Equipamentos Eletrônicos');
+insert into categoria (nome, descricao) value ('Escritório','Material de Escritório');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id) value ('Código limpo', 'Livro do autor Robert Martin', 73.44, 20, 1);
